ba206c0ba8b2af194b02020a11d270e2b0fb1da7
wwaaoonn <69627172+wwaaoonn@users.noreply.github.com>
2021-11-20 12:05:11 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fox_butler
+
+This is an app that runs on Slack that boost your daily life.
---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel  # リクエストbodyを定義するために必要
+
+app = FastAPI()
+
+
+class UserName(BaseModel):
+    first_name: str
+    last_name: str
+
+
+class User(BaseModel):
+    user_id: int
+    user_name: UserName
+
+
+@app.get("/")
+async def get_user():
+    return {
+        "user_id": 0,
+        "user_name": {
+            "first_name": "Taro",
+            "last_name": "Sato"
+        }
+    }
+
+
+@app.post("/")
+async def create_user(user: User):
+    return user
